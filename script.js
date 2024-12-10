cf77bf2e0f53cb72b3cfe7087259a7bc3263b673
Waad <waadmuaw123@gmail.com>
2024-12-10 05:17:53 -0800

    saved

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper=new swiper(".home-slider",{
+    grabcursor:true,
+    loop:true,
+    centerdslider:true,
+    autoploy:{
+    delay:7500,
+     disableOnInteraction:false,
+    },
+navigation:{
+    nextE1:".swiper-buttone-next",
+    prevE1:".swiper-button-prev",
+},
+});
